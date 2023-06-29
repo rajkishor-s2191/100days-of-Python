79d5885e770c66c687be6878de101cc9e4261c84
rajkishor-s2191 <114795532+rajkishor-s2191@users.noreply.github.com>
2023-06-29 16:59:07 +0530

    Add files via upload

---
+++ Day1/day1.py
@@ -0,0 +1,11 @@
+# print("Hello World!")
+# name=input("Enter name:")
+# print(name, len(name))
+# birthdate=int(input("Enter your birthdate "))
+# print(birthdate)
+
+print("Welcome to the Band Name Generator")
+city=input("What's name of the city you grew up in?\n")
+pet=input("What's your pet's name?\n")
+
+print("Your band name could be "+city+" "+pet)
